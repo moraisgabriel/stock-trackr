c55aec49a76a2e6e584a1a8ee1300a029fd09fb1
BrnCosta <brunooc.bc@gmail.com>
2023-10-31 00:16:06 -0300

    First commit

---
+++ src/main/java/com/testsoftware/stocktrackr/StocktrackrApplication.java
@@ -0,0 +1,13 @@
+package com.testsoftware.stocktrackr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocktrackrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StocktrackrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testsoftware/stocktrackr/StocktrackrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testsoftware.stocktrackr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocktrackrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
